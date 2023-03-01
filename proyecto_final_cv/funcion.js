f88e0cb10320f5a9f3ef852acbc6ba0fa2608800
ReiAndru <126710545+ReiAndru@users.noreply.github.com>
2023-03-01 17:23:05 -0300

    Add files via upload

---
+++ proyecto_final_cv/funcion.js
@@ -0,0 +1,13 @@
+ 
+
+/* funcion que cambia el tamaño de la foto*/
+function bigImg(x) {
+    x.style.height = "120px";
+    x.style.width = "120px";
+  }
+function normalImg(x) {
+    x.style.height = "60px";
+    x.style.width = "60px";
+  }
+
+ 
